c52d0f1b368052a97b6d70d16fb9cd85f1328cf3
Abhishek Raj <abhishekraj272@gmail.com>
2021-03-04 13:01:03 +0530

    Fixed: parseIp bug

--- helper/getIp.js
+++ helper/getIp.js
@@ -1,8 +1,8 @@
 const parseIp = (req) => {
     return (typeof req.headers['x-forwarded-for'] === 'string' && req.headers['x-forwarded-for'].split(',').shift())
-    || req.connection?.remoteAddress
-    || req.socket?.remoteAddress
-    || req.connection?.socket?.remoteAddress
+    || req.connection.remoteAddress
+    || req.socket.remoteAddress
+    || req.connection.socket.remoteAddress
 }
 
 module.exports = parseIp
