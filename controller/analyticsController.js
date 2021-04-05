ba4a7b128b1ee8c9ed455ae443d1d26ae797acb2
Abhishek Raj <abhishekraj272@gmail.com>
2021-04-05 16:02:44 +0530

    Added support for Ipv6

--- controller/analyticsController.js
+++ controller/analyticsController.js
@@ -14,23 +14,31 @@ exports.getAllCount = async (req, res) => {
 }
 
 let obfuscateIp = (ip) => {
-    ip = ip.split('.');
-    ip = ip.splice(0, ip.length-1);
-    ip = ip.join('.');
-    return ip+".***";
+    if (ip.length < 17) {
+        ip = ip.split('.');
+        ip = ip.splice(0, ip.length - 1);
+        ip = ip.join('.');
+        return ip + ".***";
+    }
+
+    ip = ip.split(':');
+    // console.log(ip)
+    ip = ip.splice(0, ip.length - 1);
+    ip = ip.join(':');
+    return ip + ".****";
 }
 
 exports.getUrlAnalytics = async (req, res) => {
     const uId = req.query.uId || null;
 
     if (!uId) {
-        res.send({pls: "Please don't exploit my resource", message: "No URL ID is provided"});
+        res.send({ pls: "Please don't exploit my resource", message: "No URL ID is provided" });
     }
 
-    let doc = await Analytics.findOne({urlId: uId}, '-_id -__v -urlId');
-    
+    let doc = await Analytics.findOne({ urlId: uId }, '-_id -__v -urlId');
+
     if (!doc) {
-        res.status(404).send({message: "Invalid Analytics ID"});
+        res.status(404).send({ message: "Invalid Analytics ID" });
     }
 
     let obfuscatedIp = doc.ip.map(data => obfuscateIp(data));
