233d675b4dc100b24b225c64217ea163f32a7bc0
Abhishek Raj <abhishekraj272@gmail.com>
2021-08-14 13:42:11 +0530

    updated analytics response

--- app.js
+++ app.js
@@ -1,40 +1,53 @@
 const express = require("express");
 const logger = require("morgan");
-var cors = require('cors')
-require('dotenv').config();
+var cors = require("cors");
+require("dotenv").config();
 
 const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.use(logger("dev"));
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+app.use(cors());
 
 const connectOptions = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     keepAlive: true,
-    useCreateIndex: true
+    useCreateIndex: true,
 };
 
-const db = require('./models');
+const db = require("./models");
 
 db.mongoose
-  .connect(process.env.MONGO_URI, connectOptions)
-  .then(() => {
-    console.log('Successfully connect to MongoDB.');
-  })
-  .catch((err) => {
-    console.error('Connection error', err);
-    process.exit();
+    .connect(process.env.MONGO_URI, connectOptions)
+    .then(() => {
+        console.log("Successfully connect to MongoDB.");
+    })
+    .catch((err) => {
+        console.error("Connection error", err);
+        process.exit();
+    });
+
+const shortRoutes = require("./routes/shortRoutes");
+const analyticsRoutes = require("./routes/analyticsRoutes");
+
+app.use("", [shortRoutes, analyticsRoutes]);
+
+io.path("/test");
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+    });
 });
 
-const shortRoutes = require('./routes/shortRoutes');
-const analyticsRoutes = require('./routes/analyticsRoutes');
-
-app.use('', [shortRoutes, analyticsRoutes]);
-
-app.listen(process.env.PORT || 3000, function () {
-    console.log("Server is running on Port: " + (process.env.PORT || 3000));
+app.listen(process.env.PORT || 3001, function () {
+    console.log("Server is running on Port: " + (process.env.PORT || 3001));
 });
--- controller/analyticsController.js
+++ controller/analyticsController.js
@@ -4,44 +4,58 @@ const URLs = db.url;
 const Analytics = db.analytics;
 
 exports.getAllCount = async (req, res) => {
-    let doc = await URLs.aggregate([{ $group: { _id: '', count: { $sum: '$count' } } }]);
+    let doc = await URLs.aggregate([
+        { $group: { _id: "", count: { $sum: "$count" } } },
+    ]);
     if (doc) {
         res.send(doc);
+    } else {
+        res.send({ statusTxt: "DB Error", statusCode: 500 });
     }
-    else {
-        res.send({ statusTxt: 'DB Error', statusCode: 500 });
-    }
-}
+};
 
 let obfuscateIp = (ip) => {
     if (ip.length < 17) {
-        ip = ip.split('.');
+        ip = ip.split(".");
         ip = ip.splice(0, ip.length - 1);
-        ip = ip.join('.');
+        ip = ip.join(".");
         return ip + ".***";
     }
 
-    ip = ip.split(':');
+    ip = ip.split(":");
     // console.log(ip)
     ip = ip.splice(0, ip.length - 1);
-    ip = ip.join(':');
+    ip = ip.join(":");
     return ip + ".****";
-}
+};
 
 exports.getUrlAnalytics = async (req, res) => {
     const uId = req.query.uId || null;
 
     if (!uId) {
-        res.send({ pls: "Please don't exploit my resource", message: "No URL ID is provided" });
+        res.send({
+            pls: "Please don't exploit my resource",
+            message: "No URL ID is provided",
+        });
     }
 
-    let doc = await Analytics.findOne({ urlId: uId }, '-_id -__v -urlId');
+    let doc = await Analytics.findOne({ urlId: uId }, "-_id -__v -urlId");
+    let urlDoc = await URLs.findById(uId);
 
     if (!doc) {
         res.status(404).send({ message: "Invalid Analytics ID" });
     }
 
-    let obfuscatedIp = doc.ip.map(data => obfuscateIp(data));
-    doc.ip = obfuscatedIp
-    res.send(doc);
-}
+    let obfuscatedIp = doc.ip.map((data) => obfuscateIp(data));
+    doc.ip = obfuscatedIp;
+    const obj = {};
+
+    obj["originalUrl"] = urlDoc.originalUrl;
+    obj["shortUrl"] = urlDoc.shortUrl;
+    obj["ip"] = doc.ip;
+    obj["os"] = doc.os;
+    obj["platform"] = doc.platform;
+    obj["browser"] = doc.browser;
+
+    res.status(200).send(obj);
+};
